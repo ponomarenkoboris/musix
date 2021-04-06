07f2077151efab6e878f8a85c62a5d1cf8433643
Boris Ponomarenko <borisponomarenko.dmi@gmail.com>
2021-04-06 15:59:59 +0300

    add some profile logic

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,7 +1,60 @@
+import React, { useState, useRef } from 'react'
+import './Profile.scss'
+// import { uploadAvatar } from "./uploadAvatar";
+
 export default function Profile() {
+    const [changes, setChanges] = useState(false)
+    const [userName, setUserName] = useState(!sessionStorage.getItem('userName') ? 'USERNAME' : sessionStorage.getItem('userName'))
+    const [userEmail, setUserEmail] = useState(!sessionStorage.getItem('userEmail') ? 'USEREMAIL' : sessionStorage.getItem('userEmail'))
+    const nameRef = useRef(null)
+    const emailRef = useRef(null)
+    const fileLoader = useRef(null)
+
+    function submitChanges() {
+        sessionStorage.setItem('userName', userName)
+        sessionStorage.setItem('userEmail', userEmail)
+        setChanges(false)
+    }
+
+    function choosePhoto() {
+        fileLoader.current.click()
+    }
+
+    function deleteUser() {
+        const result = window.confirm('Вы уверенны, что хотите удалить аккаунт?');
+        console.log('result', result)
+    }
+
     return (
         <>
-            <h1>Profile page</h1>
+            <div className="profile-container">
+                <div className="user-info-wrapper">
+                    <div className="user-avatar">
+                        {/*<h1>AVATAR</h1>*/}
+                        <input type="file" ref={fileLoader} accept=".png, .jpg, jpeg" className="file-loader"/>
+                        {changes && <button onClick={choosePhoto} className='select avatar'>Выбрать аватар...</button>}
+                    </div>
+                    <div className="user-info-settings">
+                        <div className="user-info">
+                            <div className="user-name-wrapper">
+                                <p className="user-name"><strong>Имя пользователя: </strong>
+                                    {!changes ? userName : <input ref={nameRef} type="text" value={userName} onChange={() => setUserName(nameRef.current.value)}/>}
+                                </p>
+                            </div>
+                            <div className="user-email-wrapper">
+                                <p className="user-email"><strong>Email: </strong>
+                                    {!changes ? userName : <input ref={emailRef} type="text" value={userEmail} onChange={() => setUserEmail(emailRef.current.value)}/>}
+                                </p>
+                            </div>
+                        </div>
+                        <div className="profile-settings">
+                            {!changes ? <p className="change-user-data" onClick={() => setChanges(true)}>Изменить</p> :
+                                <p className="submit-changes" onClick={submitChanges}>Сохранить изменения</p>}
+                            <p className="delete-user-data" onClick={deleteUser}>Удалить</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
